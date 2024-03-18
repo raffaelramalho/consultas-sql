b83ef7d0bec781e1b97fa3251982cb5578c36acb
DELP\rafael.rosa <21800014@aluno.cotemig.com.br>
2024-03-18 10:11:47 -0300

    Consulta para fazer mapeamento de tabelas ML

--- concatena_com_comparacao.sql
+++ concatena_com_comparacao.sql
@@ -1,6 +1,6 @@
-SELECT name, DSL_LISTA, NOM_LISTA, COD_EMPRESA, COD_LISTA
+SELECT name AS [Nome da tabela ML-Completo], DSL_LISTA as Descrição, NOM_LISTA as [Nome no fluig], CONCAT(COD_EMPRESA, FORMAT(COD_LISTA, '000')) as [Nome ML]
 FROM SYS.TABLES AS s
 JOIN META_LISTA AS m
 --Extrai as letras a partir da 5° posição e concatena com empresa+lista
-ON SUBSTRING(s.name, 5, LEN(s.name)) = CONCAT(m.COD_EMPRESA, m.COD_LISTA)
-WHERE s.name LIKE 'ML00%'
+ON CAST(RIGHT(s.name,4) AS INT) = CAST(CONCAT(COD_EMPRESA, FORMAT(COD_LISTA, '000')) AS INT)
+WHERE s.name LIKE 'ML%'
